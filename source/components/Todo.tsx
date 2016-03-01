4f66ed7f3c933b261c75daf7b0451415e82f8a96
cyrilpanicker <cyrilpanicker@gmail.com>
2016-03-01 18:52:35 +0530

    added changes

--- source/components/Todo.tsx
+++ source/components/Todo.tsx
@@ -8,29 +8,6 @@ import Link = require('./Link');
 
 import * as React from 'react';
 
-// class Link extends React.Component<any,any>{
-//     onClick(event){
-//         event.preventDefault();
-//         const {value,onClick} = this.props;
-//         onClick(value);
-//     }
-//     render(){
-//         const {value,selected,children} = this.props;
-//         if(value === selected){
-//             return (
-//                 <span className="selected-link">
-//                     {children}
-//                 </span>
-//             );
-//         }
-//         return (
-//             <a href="#" onClick={this.onClick.bind(this)} className="link">
-//                 {children}
-//             </a>
-//         );
-//     }
-// }
-
 export class Todo extends React.Component<any,any>{
     
     unsubscribe = null;
